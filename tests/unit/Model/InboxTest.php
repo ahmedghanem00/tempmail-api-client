14032bd25d5c11a910dc4829002ecfe2bd7dfcac
Ahmed Ghanem <ahmedghanem7361@gmail.com>
2024-03-23 01:41:24 +0200

    Add dump test

---
+++ tests/unit/Model/InboxTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the TempMailClient package.
+ *
+ * (c) Ahmed Ghanem <ahmedghanem7361@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ahmedghanem00\TempMailClient\Tests\unit\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class InboxTest extends TestCase
+{
+    /**
+     * @covers Inbox::retrieveAll
+     * @return void
+     */
+    public function testRetrieveAll(): void
+    {
+        $this->assertTrue(true);
+    }
+}
